ed60022b32ed79923081d813177092fb072e9047
sriramnalabolu <65815689+sriramnalabolu@users.noreply.github.com>
2024-01-15 17:27:39 -0600

    Add files via upload

---
+++ src/movielist.js
@@ -0,0 +1,67 @@
+const movieList = [
+  {
+    id: 11,
+    movie: "Chinna",
+    link: "https://maxhd.icu/Chinna%20(2023)%20720p%20HD%20-%20Bolly2Tolly.world.mp4",
+    image:
+      "https://www.bolly2tolly.net/wp-content/uploads/2023/12/Chinna-telugu-movie-online-bolly2tolly-185x275.jpg",
+  },
+  {
+    id: 10,
+    movie: "Unstoppable with NBK - Ranbir Animal Episode",
+    link: "https://vidohd.com/Unstoppable%20(2023)%20Animal%20Episode%20-%20Bolly2Tolly.dev.mp4",
+    image:
+      "https://www.bolly2tolly.net/wp-content/uploads/2023/11/Unstoppable-telugu-tvshow-ranbir-bolly2tolly-185x252.jpg",
+  },
+  {
+    id: 2,
+    movie: "Boy's Hostel",
+    link: "https://maxhd.icu/Boys%20Hostel%20(2023)%20720p%20HD%20-%20Bolly2Tolly.city.mp4",
+    image:
+      "https://www.bolly2tolly.net/wp-content/uploads/2023/08/Boys-Hostel-telugu-movie-online-bolly2tolly-185x275.jpg",
+  },
+  {
+    id: 3,
+    movie: "MAD",
+    link: "https://vidohd.com/MAD%20(2023)%20720p%20HD%20-%20Bolly2Tolly.city.mp4",
+    image:
+      "https://www.bolly2tolly.net/wp-content/uploads/2023/10/Mad-telugu-movie-online-bolly2tolly-185x275.jpg",
+  },
+  {
+    id: 4,
+    movie: "The Great Indian Suicide",
+    link: "https://vidohd.com/The%20Great%20Indian%20Suicide%20(2023)%20720p%20HD%20-%20Bolly2Tolly.city.mp4",
+    image:
+      "https://www.bolly2tolly.net/wp-content/uploads/2023/10/The-Great-Indian-Suicide-telugu-movie-online-bolly2tolly-185x275.jpg",
+  },
+  {
+    id: 5,
+    movie: "Peddakapu 1",
+    link: "https://vidohd.com/Peddakapu%201%20(2023)%20720p%20HD%20-%20Bolly2Tolly.city.mp4",
+    image:
+      "https://www.bolly2tolly.net/wp-content/uploads/2023/10/Peddha-Kapu-Part-1-telugu-movie-online-bolly2tolly-185x275.jpg",
+  },
+  {
+    id: 7,
+    movie: "Baby",
+    link: "https://vidohd.com/Baby%20(2023)%20Telugu%20720p%20HD%20-%20Bolly2Tolly.bio.mp4",
+    image:
+      "https://www.bolly2tolly.net/wp-content/uploads/2023/07/baby-telugu-movie-online-bolly2tolly-185x275.jpg",
+  },
+  {
+    id: 8,
+    movie: "Samajavaragamana",
+    link: "https://maxhd.icu/Samajavaragamana%20(2023)%20720p%20HD%20-%20Bolly2Tolly.skin.mp4",
+    image:
+      "https://www.bolly2tolly.net/wp-content/uploads/2023/06/Samajavaragamana-telugu-movie-online-bolly2tolly-185x275.jpg",
+  },
+  {
+    id: 9,
+    movie: "Vidudala 1",
+    link: "https://vidohd.com/Vidudala%20Part%201%20(2023)%20Telugu%20720p%20HD%20-%20Bolly2Tolly.wiki.mp4",
+    image:
+      "https://www.bolly2tolly.net/wp-content/uploads/2023/04/vidudala-telugu-movie-online-bolly2tolly-185x275.jpg",
+  },
+];
+
+export default movieList;
